1670dca9b8d1d3b1e1e4d2d029869cea8f5ce009
EmiRoberti77 <emi@datamotion.ai>
2024-12-16 06:47:40 +0000

    langgraph framework

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+import { ChatOpenAI } from "@langchain/openai";
+import { MemorySaver } from "@langchain/langgraph";
+import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { HumanMessage } from "@langchain/core/messages";
+
+dotenv.config();
+
+//define the langchain tools and agent
+const agentTools = [new TavilySearchResults({ maxResults: 3 })];
+const agentModel = new ChatOpenAI({ temperature: 0.2 });
+
+//init memory to persist state between graph runs
+const agentCheckPoint = new MemorySaver();
+const agent = createReactAgent({
+  llm: agentModel,
+  tools: agentTools,
+  checkpointSaver: agentCheckPoint,
+});
+
+async function invoke(question: string) {
+  const agentFinalState = await agent.invoke(
+    {
+      messages: [new HumanMessage(question)],
+    },
+    {
+      configurable: {
+        thread_id: "10",
+      },
+    }
+  );
+
+  //console.log(agentFinalState);
+  const response =
+    agentFinalState.messages[agentFinalState.messages.length - 1].content;
+  console.log(response);
+}
+
+invoke("who won the 2024 F1 champion ship");
